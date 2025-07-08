ddc50e6cf15695713c92bc94b7e18a1ba9bc4823
Андрей Седельников <nikolay.kalitkin@gmail.com>
2025-07-09 00:25:45 +0600

    скелет проекта

---
+++ apps/wikigcode/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WikigcodeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.wikigcode'
+    verbose_name = 'Инфо о G-код'
